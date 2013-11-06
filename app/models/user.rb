02709a84907f51b3c998631e0cfdd460d1d9edb5
Cristian Garner <ceegarner@hotmail.com>
2013-11-05 20:01:44 -0600

    restart

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-	 has_many :authorizations
-
-	def self.create_from_hash!(hash)
-	  create(:name => hash['user_info']['name'])
-	end
-
-end
--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '464540060329220', 'f75c73e6012693bd4c2ec11782f31b53'
-end
