c2a98e472ca452f54546fe53af3dcb1de54699d0
Cristian Garner <ceegarner@hotmail.com>
2013-12-23 12:10:58 -0600

    fixed search and creation of trips

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
 	def search
-		from_search = "%#{params[:from]}%"
+		from_search = "%#{params[:from].upcase}%"
+		to_search = "%#{params[:to].upcase}%"
 		logger.debug from_search
-		@trips = Trip.where("from_city like ?", from_search)
+		@trips = Trip.where(" upper(from_city) like? AND  upper(to_city) like ? ", from_search,to_search)
 		#TODO if trip = null hacer otro search
 	end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,7 +7,10 @@ class TripsController < ApplicationController
 
 	def create
 		if session[:user_id] != nil
-			current_user.trips.create(from_city: params[:from_city], from_country: params[:from_country], from_when: params[:from_when], to_city: params[:to_city],to_country: params[:to_country], to_when: params[:to_when])
+			logger.debug "WTFFFF"
+			logger.debug params[:from_when]
+			logger.debug params[:to_when]
+			current_user.trips.create(from_city: params[:from_city], from_country: params[:from_country], from_when: Date.parse(params[:from_when]), to_city: params[:to_city],to_country: params[:to_country],to_when:  Date.parse(params[:to_when]))
 			#TODO: mandar mensaje para el usuario
 		end
 		redirect_to root_url
