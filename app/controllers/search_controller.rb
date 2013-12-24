26a82b7933a8b7457c345619f9cb72457e41fba4
Cristian Garner <ceegarner@hotmail.com>
2013-12-24 13:14:20 -0600

    Added dates on Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,8 @@ class SearchController < ApplicationController
 		if params[:from] != "" && params[:to] != ""
 			from_search = "%#{params[:from].upcase}%"
 			to_search = "%#{params[:to].upcase}%"
-			@trips = Trip.where(" upper(from_city) like? AND  upper(to_city) like ? ", from_search,to_search)
+			trip_date = Date.parse(params[:date])
+			@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ? AND from_when >= DATE(CURRENT_DATE) AND to_when >= ? AND from_when <= ?", from_search,to_search,trip_date,trip_date)
 			#TODO if trip = null hacer otro search
 		end
 	end
