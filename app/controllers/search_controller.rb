7a065b65bce3a083090bead203a748f27f5b7811
Cristian Garner <ceegarner@hotmail.com>
2014-03-31 10:23:54 -0600

    changed facebook tokens

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
 	def search
 		@trips = [];
-		if params[:from] != "" && params[:to] != "" && params[:date] != ""
+		if params[:from] != "" && params[:to] != "" 
 			from_search = "%#{params[:from].upcase}%"
 			to_search = "%#{params[:to].upcase}%"
-			trip_date = Date.parse(params[:date])
-			@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ? AND from_when >= DATE(CURRENT_DATE) AND to_when >= ? AND from_when <= ?", from_search,to_search,trip_date,trip_date)
+			if !params[:date].blank?
+				trip_date = Date.parse(params[:date])
+				@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ? AND from_when >= DATE(CURRENT_DATE) AND to_when >= ? AND from_when <= ?", from_search,to_search,trip_date,trip_date)
+			else
+				@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ?", from_search,to_search)
+			end
 			#TODO if trip = null hacer otro search
 		end
 	end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '738099589550780' , '13ef58d035c2801bd862ade8b848cd26', {image_size: 'large' , client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
+  provider :facebook, ENV['FACEBOOK_KEY'] , ENV['FACEBOOK_SECRET'], {image_size: 'large' , client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
  
 end
