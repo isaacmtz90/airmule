f366ffa114c6125b9696500b1dce3aa42ba2de28
Cristian Garner <crisgarner@Cristians-MacBook-Air.local>
2014-06-16 18:41:48 -0700

    search bug fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,15 @@
 class SearchController < ApplicationController
 	def search
 		@trips = [];
+
 		if params[:from] != "" && params[:to] != "" 
 			from_search = "%#{params[:from].upcase}%"
 			to_search = "%#{params[:to].upcase}%"
+
 			if !params[:date].blank?
+				
 				trip_date = Date.parse(params[:date])
-				@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ? AND from_when >= DATE(CURRENT_DATE) AND to_when >= ? AND from_when <= ?", from_search,to_search,trip_date,trip_date)
+				@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ? AND to_when >= ? AND from_when <= ?", from_search,to_search,trip_date,trip_date)
 			else
 				@trips = Trip.where(" upper(from_city) like ? AND upper(to_city) like ?", from_search,to_search)
 			end
